7a9b17fa4786e71d2bbfcd7ffc21930dbc56c135
Manas Kumar <themanaskumar1@gmail.com>
2024-10-27 21:57:23 +0530

    Adding all the files to github

---
+++ script.js
@@ -0,0 +1,60 @@
+var textarea = document.getElementById("newTask");
+var addTaskButton = document.getElementById("addTaskButton");
+textarea.addEventListener("input", () => {
+    if(textarea.value === ""){
+        addTaskButton.disabled = true;
+        addTaskButton.classList.add("disabled");
+        addTaskButton.classList.remove("enabled");
+    }else{
+        addTaskButton.disabled = false;
+        addTaskButton.classList.remove("disabled");
+        addTaskButton.classList.add("enabled");
+    }
+});
+
+textarea.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        addTask();
+    }
+  });
+
+function addTask(){
+    let pending = document.getElementById("pendingTasks")
+    let outerdiv = document.createElement("div")
+    outerdiv.classList.add("tasks");
+    let para = document.createElement("p");
+    para.textContent = textarea.value;
+    textarea.value = "";
+    addTaskButton.classList.add("disabled");
+    addTaskButton.classList.remove("enabled");
+    let button = document.createElement("button");
+    button.classList.add("completed");
+    button.setAttribute("onclick", "taskCompleted(this)");
+    button.textContent = "Mark Complete";
+    outerdiv.appendChild(para)
+    outerdiv.appendChild(button);
+    pending.appendChild(outerdiv);
+}
+
+function taskCompleted(button){
+    let task = button.parentElement;
+    let taskContent = task.querySelector("p").textContent;
+    let completedTasks = document.getElementById("completedTasks");
+    let innerdiv = document.createElement("div");
+    innerdiv.classList.add("tasks");
+    let para = document.createElement("p");
+    para.textContent = taskContent;
+    let deleteButton = document.createElement("button");
+    innerdiv.appendChild(para);
+    innerdiv.appendChild(deleteButton);
+    deleteButton.setAttribute("onclick", "deleteTask(this)");
+    completedTasks.appendChild(innerdiv);
+    deleteButton.textContent = "Delete";
+    task.remove();
+}
+
+function deleteTask(button){
+    let parent = button.parentElement;
+    parent.remove();
+}
